f4c407122015399efd5c8890c356067665f23036
Asa Paparo <asapaparo@gmail.com>
2022-11-19 17:44:53 -0500

    remade project, added wpilib, junit, spotless, workflows

---
+++ src/main/java/frc/sciborgs/scilib/Thing.java
@@ -0,0 +1,7 @@
+package frc.sciborgs.scilib;
+
+public class Thing {
+    public double getNumber() {
+        return 0.2;
+    }
+}
---
+++ src/test/java/frc/sciborgs/scilib/ThingTest.java
@@ -0,0 +1,11 @@
+package frc.sciborgs.scilib;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThingTest {
+    @Test void someLibraryMethodReturnsTrue() {
+        Thing thing = new Thing();
+        assertEquals(thing.getNumber(), 0.2);
+    }
+}
