7b4628a064c2e159af72e9b09b9ea17901385b90
CalciumNitrate <74282621+CalciumNitrate@users.noreply.github.com>
2022-12-11 03:13:14 -0500

    innit for Encoder + Motor Config

---
+++ src/main/java/frc/sciborgs/scilib/config/EncoderConfig.java
@@ -0,0 +1,32 @@
+package frc.sciborgs.scilib.config;
+
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.Encoder;
+
+public class EncoderConfig {
+    RelativeEncoder encoder;
+    //if you wanna use the class only to configure existing encoders
+    public EncoderConfig(){}
+    public EncoderConfig(RelativeEncoder encoder, double gearRatio){
+        this.encoder = encoder;
+        this.encoder.setVelocityConversionFactor(gearRatio);
+
+    }
+    public EncoderConfig(RelativeEncoder encoder, double gearRatio, int wheelCircumfrence){
+        this(encoder, gearRatio);
+        this.encoder.setPositionConversionFactor(wheelCircumfrence * gearRatio);
+    }
+    public void encoderConfigurator(RelativeEncoder encoder, double gearRatio, boolean isInverted){
+        encoder.setVelocityConversionFactor(gearRatio);
+        encoder.setInverted(isInverted);
+    }
+    public void encoderConfigurator(RelativeEncoder encoder, double gearRatio){
+        encoderConfigurator(encoder, gearRatio, false);
+    }
+    public RelativeEncoder getEncoder(){
+        return encoder;
+    }
+    
+
+}
--- src/main/java/frc/sciborgs/scilib/config/MotorConfig.java
+++ src/main/java/frc/sciborgs/scilib/config/MotorConfig.java
@@ -1,9 +1,34 @@
 package frc.sciborgs.scilib.config;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-public record MotorConfig() {
-    public static CANSparkMax getCANSparkMAX() {
-        return null;
+import edu.wpi.first.wpilibj.Encoder;
+
+
+public class MotorConfig {
+    private CANSparkMax canSparkMax;
+    private RelativeEncoder encoder;
+    private EncoderConfig eConfig = new EncoderConfig();
+    
+    public MotorConfig(int id, MotorType motorType, boolean isInverted){
+        canSparkMax = new CANSparkMax(id, motorType);
+        canSparkMax.setInverted(isInverted);
+        eConfig.encoderConfigurator(canSparkMax.getEncoder(), 10.71);
+        encoder = canSparkMax.getEncoder();
     }
+    public MotorConfig(int id, MotorType motorType){
+        this(id, motorType, false);
+    }
+    public CANSparkMax getCanSparkMax(){
+        return canSparkMax;
+    }
+    public void setVoltage(int Voltage){
+        canSparkMax.setVoltage(Voltage);
+    }
+    public double getVelocity(){
+        return encoder.getVelocity();
+    }
+    
 }
