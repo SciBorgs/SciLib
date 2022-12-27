5deacb97f72e456740b6967121bb3eaa8c2b67c7
Asa Paparo <asapaparo@gmail.com>
2022-12-27 15:46:22 -0600

    fixed building, rolled back wpilib, ported MotorConfig to kotlin

--- src/main/java/frc/sciborgs/scilib/config/Config.kt
+++ src/main/java/frc/sciborgs/scilib/config/Config.kt
@@ -1,1 +1,12 @@
+@file:JvmName("Config")
+
+package frc.sciborgs.scilib.config
+
+import edu.wpi.first.math.controller.PIDController
+
 data class PIDConstants(val kp: Double = 0.0, val ki: Double = 0.0, val kd: Double = 0.0)
+
+data class FFConstants(val ks: Double = 0.0, val kv: Double = 0.0, val ka: Double = 0.0)
+
+fun createPID(constants: PIDConstants): PIDController =
+    PIDController(constants.kp, constants.ki, constants.kd)
--- src/test/java/frc/sciborgs/scilib/config/ConfigTest.java
+++ src/test/java/frc/sciborgs/scilib/config/ConfigTest.java
@@ -1,14 +1,13 @@
 package frc.sciborgs.scilib.config;
 
-import com.revrobotics.CANSparkMax;
 import org.junit.jupiter.api.Test;
 
 class ConfigTest {
   @Test
   void nothing() {
-    MotorConfig cfg = new MotorConfig();
-    cfg.setInverted(true);
-    CANSparkMax motor = cfg.getCanSparkMax(1);
+    // MotorConfig cfg = new MotorConfig();
+    // cfg.setInverted(true);
+    // CANSparkMax motor = cfg.getCanSparkMax(1);
     assert true;
   }
 }
